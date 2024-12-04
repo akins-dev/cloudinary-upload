3d89a69690b63326356b42c022f72b095b4ea38f
akins-dev <bobemmanuel503@gmail.com>
2024-12-04 20:30:41 +0100

    chore: make upload work and customize button

--- components/customized-widget.tsx
+++ components/customized-widget.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { CldImage, CldUploadButton } from 'next-cloudinary';
+
 import { useState } from 'react';
+import { Upload } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
 
 
 const CustomizedCloudinaryWidget = () => {
@@ -12,15 +16,17 @@ const CustomizedCloudinaryWidget = () => {
 
         <>
             <CldUploadButton
-                onUpload={(result) => {
+                onSuccess={(result) => {
                     setResource(result?.info);  // { public_id, secure_url, etc }
                 }}
                 options={{ sources: ['local', 'camera',] }}
                 // make sure your preset is set to unsigned in your cloudinary settings
-                uploadPreset="hms_preset"
-                className='text-white bg-[#132096] text-center gap-x-2 w-[500px] py-2 rounded-full hover:bg-primary/80'
+                uploadPreset="replace_with_your_reset"
             >
-                Upload File(s) - Less than 1000kb
+                <Button >
+                    <Upload />
+                    Upload
+                </Button>
             </CldUploadButton>
             {
                 resource && (
